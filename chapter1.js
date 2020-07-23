9fe9f026ef2cb979f20e7c986ed224d63e91fd1f
syed sheraz ali <55004534+syed-sheraz-ali@users.noreply.github.com>
2020-07-23 22:31:08 +0500

    Chapter 1

---
+++ chapter1.js
@@ -0,0 +1,14 @@
+// chapter 1 : Task#1 & 2
+javascript:alert("Error! Please enter a valid password" );
+
+// chapter 1 : Task#3
+alert('Welcome to JS land \n Happy Coding');
+
+// chapter 1 : Task#4
+
+alert('Welcome to JS land...');
+alert('Happy Coding!\n', prompt('prevent this page to creating additional dialogue box'))
+
+// chapter 1 : Task#5
+
+console.log("Hello.. I can run JS through my web browser's console ")
